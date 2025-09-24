d53dd84c72681a946334e0a0d4a9c53fc3d7f168
AbdulAhad87522 <ahadbahi263@gmail.com>
2025-09-24 12:33:32 +0500

    .

--- week1/Lab1.py
+++ week1/Lab1.py
@@ -76,5 +76,83 @@
 
 #task6 (b)
 
-def sumrecursively(arr):
-    
+# def sumrecursively(arr):
+
+#task7
+
+# def columnwisesum(Mat):
+#     row = len(Mat)
+#     col = len (Mat[0])
+#     ans = [0] * col
+#     for i in range(col):
+#         for j in range(row):
+#             ans[i] = ans[i] + Mat[j][i]
+#     return ans   
+        
+        
+# Mat = [1,2,3],[4,5,6],[1,2,3]
+# print(columnwisesum(Mat))
+
+
+# def rowwisesum(Mat):
+#     row = len(Mat[0])
+#     col = len(Mat)
+#     ans = [0] * row
+#     for i in range(col):
+#         for j in range(row):
+#             ans[i] = ans[i] + Mat[i][j]
+#     return ans
+
+
+# Mat = [1,2,3],[4,5,6],[1,2,3]
+# print(rowwisesum(Mat))
+
+#task 8
+
+# def sortedmerge(arr1 , arr2):
+#     for i in range(len(arr2)):
+#         arr1.append(arr2[i])
+#     for i in range(len(arr1) - 1):
+#         for j in range((len(arr1) - i - 1)):
+#             if(arr1[j] > arr1[j+1]):
+#                 arr1[j] , arr1[j+1] = arr1[j+1] , arr1[j]
+#     return arr1
+
+# arr1 = [1 ,2,3,4,4]
+# arr2 = [6,4,2,2]
+# print(sortedmerge(arr1 , arr2))
+
+#task 9
+
+# def Palindromerecursive(str):
+#     if(len(str) < 1):
+#         return str
+#     else:
+#         return Palindromerecursive(str[1:]) + str[0]
+        
+# str = "ahad"
+# print(Palindromerecursive(str))
+
+#task 10
+
+def sort10(arr):
+    arr.sort()
+    neg_arr = []
+    pos_arr = []
+    for i in range(len(arr)):
+        if(arr[i] < 0):
+            neg_arr.append(arr[i])
+        else:
+            pos_arr.append(arr[i])
+    # return neg_arr
+    i = 0 
+    j = 0
+    result = []
+    while(i < len(neg_arr) or j< len(pos_arr)):
+        result.append(neg_arr[i])
+        result.append(pos_arr[j])
+        i = i + 1
+        j = j + 1
+    return result
+arr = [1,5,7,3,3,5,12,33,2,-20,-9,-9,-98]
+print(sort10(arr))
