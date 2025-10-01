3677f3ade51db8c10d1a79c2ed9e98d2e049fb22
AbdulAhad87522 <ahadbahi263@gmail.com>
2025-10-01 18:28:03 +0500

    ..

--- week2/2024-cs-40 lab2/bubble.py
+++ week2/2024-cs-40 lab2/bubble.py
@@ -5,8 +5,8 @@ import random
 
 def buublesort(arr , st , end):
     n= end - st
-    for i in range(n - 1):
-        for j in range( st ,end - i - 1):
+    for i in range(0,n-1):
+        for j in range( st ,end-i-1):
             if(arr[j] > arr[j + 1]):
                 temp = arr[j]
                 arr[j] = arr[j + 1]
@@ -14,15 +14,16 @@ def buublesort(arr , st , end):
     return arr
 
 
-arr = [random.randint(0, 1000) for i in range(30000)]
+arr = [random.randint(0, 30) for i in range(30)]
 start_time = time.time()
 sorted_array = buublesort(arr , 0 , len(arr))
+print (sorted_array)
 
-f = open(file="bubblesort.txt" , mode="w")
+""" f = open(file="bubblesort.txt" , mode="w")
 for i in sorted_array:
     f.write(str(i) + "\n")
 end_time= time.time()
 
 run_time= end_time - start_time
 
-print(run_time)
+print(run_time)"""
